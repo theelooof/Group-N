93f7bce23ee11100147dd460dd7c9d63ae25b658
i2aoul <dankert.raoul@gmail.com>
2023-09-21 04:24:19 +0200

    finished all the features

--- Boardrepresentation/mill_board.py
+++ Boardrepresentation/mill_board.py
@@ -9,6 +9,8 @@ class Node:
         self.right = None
         self.up = None
         self.down = None
+        self.turn=0
+        self.pieces=None
 
     def __repr__(self):
         return f'Node(id={self.id}, occupied={self.occupied}, player={self.player}'
@@ -28,24 +30,24 @@ class MillBoard:
             3: {"right": 4, "down": 10},
             4: {"left": 3, "right": 5, "up": 1, "down": 7},
             5: {"left": 4, "down": 13},
-            6: {"right": 7, "down": 15},
+            6: {"right": 7, "down": 11},
             7: {"left": 6, "right": 8, "up": 4},
-            8: {"left": 7, "up": 5},
+            8: {"left": 7, "up": 11},
             9: {"right": 10, "up": 0, "down": 21},
             10: {"left": 9, "right": 11, "up": 3, "down": 18},
-            11: {"left": 10, "up": 2, "down": 23},
-            12: {"right": 13, "up": 8, "down": 20},
-            13: {"left": 12, "right": 14, "up": 5, "down": 17},
-            14: {"left": 13, "up": 2, "down": 22},
-            15: {"right": 16, "up": 6},
-            16: {"left": 15, "right": 17, "up": 7, "down": 19},
-            17: {"left": 16, "up": 13},
+            11: {"left": 10, "up": 6, "down": 15},
+            12: {"right": 13, "up": 8, "down": 17},
+            13: {"left": 12, "right": 14, "up": 5, "down": 20},
+            14: {"left": 13, "up": 2, "down": 23},
+            15: {"right": 16, "up": 11},
+            16: {"left": 15, "right": 17, "down": 19},
+            17: {"left": 16, "up": 12},
             18: {"right": 19, "up": 10},
-            19: {"left": 18, "right": 20, "up": 16},
-            20: {"left": 19, "up": 12},
+            19: {"left": 18, "right": 20, "up": 16, "down":22},
+            20: {"left": 19, "up": 13},
             21: {"right": 22, "up": 9},
-            22: {"left": 21, "right": 23, "up": 14},
-            23: {"left": 22, "up": 11}
+            22: {"left": 21, "right": 23, "up": 19},
+            23: {"left": 22, "up": 14}
         }
         for node_id, neighbors_ids in neighbors.items():
             node = self.node[node_id]
@@ -117,21 +119,21 @@ class MillBoard:
         primary_node = getattr(node, primary_dir)
         
         if(primary_node==None):
-           return False
+           return None
         if(primary_dir== secondary_dir):
             secondary_node = getattr(primary_node, secondary_dir) 
         else:
             secondary_node = getattr(node, secondary_dir) 
         if(secondary_node ==None):
-           return False
+           return None
         
         
         nodes = [n for n in [node, primary_node, secondary_node] if n]
         if len(nodes) != 3:
-            return False
+            return None
         if all(n.occupied for n in nodes) and len(set(n.player for n in nodes)) == 1:
-            return True 
-        return False
+            return nodes
+        return None
     # checks mill
     def check_for_mill(self, position):
         node = self.node[ID_MAPPING.get(position.upper(), None)]
@@ -143,17 +145,76 @@ class MillBoard:
         horizontal_muehle = (self.check_muehle_in_direction(node, "left", "left") or 
                              self.check_muehle_in_direction(node, "left", "right") or 
                              self.check_muehle_in_direction(node, "right", "right"))
-        
-        return vertical_muehle or horizontal_muehle
-        
+        if(vertical_muehle!=None):
+            return vertical_muehle
+        if(horizontal_muehle!=None):
+            return horizontal_muehle
+        return None
+    
 
+    def is_valid_move(self,input_position):
+        # Überprüfe, ob die Eingabe im ID_MAPPING ist
+        if input_position in ID_MAPPING:
+            node_id = ID_MAPPING[input_position]
+            node = self.node[node_id]
+            
+            # Überprüfe, ob der Knoten bereits belegt ist
+            if not node.occupied:
+                return True
+            else:
+                print(f"The position {input_position} is already occupied.")
+        else:
+            print(f"Invalid input: {input_position} is not a valid position.")  
+        return False    
+    
+    def get_next_possible_moves(self,input_move):
+        # Check if the input move is in ID_MAPPING
+        if input_move in ID_MAPPING:
+            node_id = ID_MAPPING[input_move]
+            current_node = self.node[node_id]
+
+            # Create a list to store the next possible moves
+            next_possible_moves = []
+
+            # Check neighboring nodes for possible moves
+            neighbors = [current_node.left, current_node.right, current_node.up, current_node.down]
+            for neighbor_node in neighbors:
+                if neighbor_node is not None and not neighbor_node.occupied:
+                    # Find the position associated with the neighbor_node
+                    for position, node_id in ID_MAPPING.items():
+                            next_possible_moves.append(position)
+
+            # Convert the list of possible moves to a comma-separated string
+            next_moves_text = ", ".join(next_possible_moves)
+
+            return next_moves_text
+        else:
+            print(f"Invalid input: {input_move} is not a valid position.")
+            return ""
     #TODO: Check if Mill is formed and remove a stone of the opponent
-        
+
+    def remove_player(self, position: str, player: str):
+        node_id = ID_MAPPING.get(position.upper(), None)
+        if node_id is None:
+            print(f"Invalid position: {position} is not a valid position.")
+            return False
+
+        if not self.node[node_id].occupied:
+            print(f"No player at position {position} to remove.")
+            return False
+
+        if self.node[node_id].player == player:
+            print(f"Player {player} cannot remove a piece at position {position}.")
+            return False
+
+        self.node[node_id].player = None
+        self.node[node_id].occupied = False 
+        return True  
     # TODO: Check if Player has only 3 stones left and can move to any free space     
 
 board = MillBoard()
 
-def draw_millboard(board:MillBoard ):
+def draw_millboard(self,board:MillBoard ):
     global A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X
     letter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"]
     for i, letter in enumerate(letter):
@@ -175,8 +236,9 @@ def draw_millboard(board:MillBoard ):
     lower_second_line = f'F  │  {S}────{T}────{U}  │'       + f'    | Phase 3: Phase 3 begins when one of the players has 3 pieces left. '
     inner_lines_12 =    f'   │       │       │'             + f'    |          Here the player is allowed to move their pieces to any vacant '
     lower_first_line =  f'G  {V}───────{W}───────{X }'      + f'    |          intersection.'
-    end_line = f'                   '                       +f'     |-------------------------------------------------------------------------------'
-
+    end_line =          f'                    '             + f'    |-------------------------------------------------------------------------------'
+    round_line=          f'Player:{self.players[self.turn % 2]} Pieces:{self.pieces[self.turn % 2]} Round:{self.turn}'
+    exit_line=f'Enter "exit" to exit the game'
     # Assembly of the Boards
     board = [
         number_line,
@@ -193,19 +255,56 @@ def draw_millboard(board:MillBoard ):
         lower_second_line,
         inner_lines_12,
         lower_first_line,
-        end_line
+        end_line,
+        round_line,
+        exit_line
     ]
     
     print('\n'.join(board))
     
-    
-'''
-board.occupy_node("A1","X")
-board.move_player("A1","A4")
-board.occupy_node("A7","O")
-board.move_player("A7","D5")
-draw_millboard(board)
-board.occupy_node("D1","X")
-board.occupy_node("E5","O")
-draw_millboard(board)'''
+def announce_winner(self):
+    text=[player_1_winner_text,player_2_winner_text]
+    winner_text=text[(self.turn +1) % 2]
+    print('\n'.join(winner_text))
 
+
+
+player1_1=     f'  _____  _                         __  __          ___              '         
+player1_2=     f' |  __ \| |                       /_ | \ \        / (_)             '
+player1_3=     f' | |__) | | __ _ _   _  ___ _ __   | |  \ \  /\  / / _ _ __  ___    ' 
+player1_4=     f' |  ___/| |/ _` | | | |/ _ \ |__|  | |   \ \/  \/ / | | |_ \/ __|   '
+player1_5=     f' | |    | | (_| | |_| |  __/ |     | |    \  /\  /  | | | | \__ \   '
+player1_6=     f' |_|    |_|\__,_|\__, |\___|_|     |_|     \/  \/   |_|_| |_|___/   '
+player1_7=     f'                  __/ |                                             '
+player1_8=     f'                 |___/                                              '
+
+player_1_winner_text=[
+    player1_1,
+    player1_2,
+    player1_3,
+    player1_4,
+    player1_5,
+    player1_6,
+    player1_7,
+    player1_8
+]
+
+player2_1= f'  _____  _                         ___   __          ___               '
+player2_2= f' |  __ \| |                       |__ \  \ \        / (_)              '
+player2_3= f' | |__) | | __ _ _   _  ___ _ __     ) |  \ \  /\  / / _ _ __  ___     '
+player2_4= f' |  ___/| |/ _` | | | |/ _ \ |__|   / /    \ \/  \/ / | | |_ \/ __|    '
+player2_5= f' | |    | | (_| | |_| |  __/ |     / /_     \  /\  /  | | | | \__ \    '
+player2_6= f' |_|    |_|\__,_|\__, |\___|_|    |____|     \/  \/   |_|_| |_|___/    '
+player2_7= f'                 __/ |                                                 '
+player2_8= f'                |___/                                                  '
+
+player_2_winner_text=[
+    player2_1,
+    player2_2,
+    player2_3,
+    player2_4,
+    player2_5,
+    player2_6,
+    player2_7,
+    player2_8
+]
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from Boardrepresentation.mill_board import MillBoard
 from Boardrepresentation.mill_board import draw_millboard
+from Boardrepresentation.mill_board import announce_winner
 
 class Game:
     def __init__(self):
@@ -7,38 +8,108 @@ class Game:
         self.gameOver = False
         self.turn = 0
         self.players = ["X", "O"]
+        self.pieces=[3,3]
+        self.last_move=None
     
     #Input moves for the game
     
     def input_move(self):
         while True:
             try:
-                move = input("Enter your move (e.g., 'A1 B2'): ").strip().upper()
-                if len(move) == 5 and move[2] == ' ':
-                    start_position = move[:2]
-                    end_position = move[3:]
-                    #TODO: Check if the move is valid and apply it to the board
-                    if True:
-                        self.board.move_player_phase_two(start_position, end_position)
-                        self.gameOver = self.board.check_for_mill(move)
+                
+                if self.turn>18:
+                    move1 = input("Enter your first move (e.g., 'A1'): ").strip().upper()
+                    if(move1=="undo"):
                         return
-                    else:
-                        print("Invalid move. Try again.")
-                elif len(move) == 2:
-                    self.board.move_player_phase_one(move, self.players[self.turn % 2])
-                    self.gameOver = self.board.check_for_mill(move)
+                    if(move1=="EXIT"):
+                        self.gameOver=True
+                        return
+                    while(not(self.board.is_valid_move(move1))):
+                        move1 = input("Choose the piece you want to move (e.g., 'A1'): ").strip().upper()
+                        next_moves= self.board.get_next_possible_moves(move1)
+                        print(f"Next possible moves from {move1}: {next_moves}")
+
+                        if(move1=="undo"):
+                            return
+                        if(move1=="EXIT"):
+                            self.gameOver=True
+                            return
+
+                    move2 = input("Choose the position you want to move the piece to (e.g., 'A4'): ").strip().upper()
+                    if(move2=="EXIT"):
+                        self.gameOver=True
+                        return
+                    while(not(self.board.is_valid_move(move2))):
+                        move2 = input("Choose the position you want to move it to (e.g., 'A4'): ").strip().upper()
+                        if(move2=="EXIT"):
+                            self.gameOver=True
+                            return
+                    #TODO: Check if the move is valid and apply it to the board
+                    
+                    self.board.move_player_phase_two(move1, move2)
+                    self.last_move=move2
                     return
                 else:
-                    print("Invalid input format. Use 'A1 B2' format.")
+                    move0 = input("Enter your move (e.g., 'A1, A4,...'): ").strip().upper()
+                    if(move0=="EXIT"):
+                        self.gameOver=True
+                        return
+                    while(not(self.board.is_valid_move(move0))):
+                        move0 = input("Enter your move (e.g., 'A1, A4,...'): ").strip().upper()
+                        if(move0=="EXIT"):
+                            self.gameOver=True
+                            return
+                    self.board.move_player_phase_one(move0, self.players[self.turn % 2])
+                    self.last_move=move0
+                    return
+                    
             except KeyboardInterrupt:
-                exit()
+                EXIT()
+
+    def mill(self):
+        mill_nodes=self.board.check_for_mill(self.last_move)
+        mill = False if mill_nodes is None else True 
+        if(mill):
+            draw_millboard(self,self.board)
+        if(mill_nodes!=None):
+            print(f"Mill is build by{mill_nodes[0].id,mill_nodes[1].id,mill_nodes[2].id} ")
+        while(mill):
+            remove_piece = input("Enter the position of the piece that you want to remove: ").strip().upper()
+            mill=not(self.board.remove_player(remove_piece,self.players[self.turn % 2]))
+            self.pieces[self.turn % 2]-=1
+            if(remove_piece=="EXIT"):
+                self.gameOver=True
+                return
+        if(self.pieces[self.turn % 2]<3):
+               announce_winner(self)
+               self.restart()
+        return
+    def restart(self):
+        restar_text = input("\n Do you want to pkay again yes/no \n").strip().upper()
+        if(restar_text=="YES"):
+           self.board= MillBoard()
+           self.gameOver = False
+           self.turn = 0
+           self.pieces=[3,3]
+        if(restar_text=="NO"):
+           self.gameOver=True
 
 
     #Game loop
     def game_loop(self):
         while not self.gameOver:
-            draw_millboard(self.board)
-            self.input_move()
+            self.board.players=self.players
+            self.board.turn=self.turn
+            self.board.pieces=self.pieces
+            if(self.turn==0):
+               draw_millboard(self,self.board)           
+            else:
+               self.input_move()
+               if(self.turn>0):
+                  self.mill()
+               draw_millboard(self,self.board)
+               
             self.turn += 1
 
 
+   
